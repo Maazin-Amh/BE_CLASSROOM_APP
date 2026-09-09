5e24f9aa35c30ea7e9bfd6f962c04cd7051bdea9
Ariiq <ariiqmaazin@gmail.com>
2026-09-09 21:59:51 +0700

    update

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,4 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
@@ -7,7 +15,9 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
+
+  synchronize: false,
   logging: true,
 };
