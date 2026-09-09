0ce41bbc59d2bd7166655a19c8b71801943f26ca
Ariiq <ariiqmaazin@gmail.com>
2026-09-09 14:14:57 +0700

    update

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,6 +8,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: false,
+  synchronize: true,
   logging: true,
 };
--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     }),
   );
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  await app.listen(4400);
+  await app.listen(process.env.PORT || 4400);
 }
 bootstrap();
