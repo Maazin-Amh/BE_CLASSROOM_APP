7b5f6df32f9b5a7c5f86d1884568a8a29ad36f57
Ariiq <ariiqmaazin@gmail.com>
2026-09-09 23:41:27 +0700

    update

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,7 +9,7 @@ import BaseResponse from '../../utils/response/base.response';
 import { LoginDto, RegisterDto, ResetPasswordDto } from './auth.dto';
 import { ResponseSuccess } from 'src/interface';
 import { JwtService } from '@nestjs/jwt';
-import { jwt_config } from 'src/config/jwt.config';
+import { jwt_config } from '../../config/jwt.config';
 import { Repository } from 'typeorm';
 import { randomBytes } from 'crypto';
 import { compare, hash } from 'bcrypt';
